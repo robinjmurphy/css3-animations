382fa4cd3815438623ee0da572c9324e79e42d9d
Robin Murphy <robin@robinmurphy.co.uk>
2012-12-28 23:15:34 +0000

    Basic functionality with addEventListener

---
+++ js/tabpanel.js
@@ -0,0 +1,80 @@
+(function($){
+    'use strict'
+
+    var domReady,
+        tabPanels;
+
+    /* Check if DOM is finished loading from document.readyState */
+    domReady = function ready(e){/in/.test(document.readyState)?setTimeout(function(){ready(e)},40):e()}
+
+    tabPanels = function () {
+        var dropdown = $('.dropdown')[0],
+            tabs = $('.masthead a'),
+            pages = $('.page'),
+            masthead = $('.masthead')[0];
+
+        function togglePage (page) {
+            if (page.className.indexOf('active') > -1) {
+                closePage(page);
+            }
+            else {
+                openPage(page);
+            }
+        }
+
+        function openPage (page) {
+            closeAllPages();
+            page.className = 'page active';
+            toggleDropdown();
+            setHeightOfDropdown(page.offsetHeight);
+        }
+
+        function closePage (page) {
+            page.className = 'page';
+            toggleDropdown();
+        }
+
+        function closeAllPages () {
+            for (var i = pages.length - 1; i >= 0; i--) {
+                pages[i].className = 'page'
+            };
+        }
+
+        function toggleDropdown () {
+            if ($('.active')[0] === undefined) {
+                closeDropdown();
+            }
+            else {
+                openDropdown();
+            }
+        }
+
+        function openDropdown () {
+            masthead.className = 'masthead open';
+        }
+
+        function closeDropdown () {
+            masthead.className = 'masthead';
+            dropdown.style.height = 0;
+        }
+
+        function setHeightOfDropdown (pageHeight) {
+            dropdown.style.height = pageHeight + 'px';
+        }
+
+        return {
+            bindEvents: function () {
+                for (var i = tabs.length - 1; i >= 0; i--) {
+                    tabs[i].addEventListener("click", function(e){
+                        togglePage($('#' + e.srcElement.getAttribute('aria-controls'))[0]);
+                        e.returnValue = false;
+                    });
+                }
+            }
+        }
+    }
+
+    domReady(function(){
+        tabPanels().bindEvents();
+    });
+})(window.Sizzle);
